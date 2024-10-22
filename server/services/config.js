d18deb4734aa00d2012cf2545b22eac980276c06
nshenderov <nkshenderov@gmail.com>
2024-10-22 19:29:53 +0300

    fix: correct config file reading logic to allow environment configurations, #171

--- server/services/config.js
+++ server/services/config.js
@@ -1,25 +1,43 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 module.exports = ({ strapi }) => {
+  const readConfigFile = () => {
+    const appDir = process.cwd();
+    const isTSProject = fs.existsSync(`${appDir}/dist`);
+    const envName = process.env.NODE_ENV;
+
+    const cfgDir = isTSProject ? `${appDir}/dist/config` : `${appDir}/config`;
+    const cfgFileName = 'ckeditor.js';
+
+    const envFilePath = `${cfgDir}/env/${envName}/${cfgFileName}`;
+    const baseFilePath = `${cfgDir}/${cfgFileName}`;
+
+    if (fs.existsSync(envFilePath)) {
+      return fs.readFileSync(envFilePath, 'utf8');
+    } else if (fs.existsSync(baseFilePath)) {
+      return fs.readFileSync(baseFilePath, 'utf8');
+    } else {
+      return null;
+    }
+  };
+
+  const trimConfig = (str) => {
+    for (const func of ['const CKEConfig', 'function CKEConfig']) {
+      const idx = str.indexOf(func);
+      if (idx >= 0) {
+        return str.substring(idx) + `\nglobalThis.SH_CKE_CONFIG = CKEConfig()`;
+      }
+    }
+  };
+
   return {
     getConfig() {
-      const appDir = process.cwd();
+      const configFileContent = readConfigFile();
+      const config = configFileContent && trimConfig(configFileContent);
 
-      const fileName = "ckeditor";
-
-      for (const ext of ["js", "ts"]) {
-        const filePath = `${appDir}/config/${fileName}.${ext}`;
-        if (fs.existsSync(filePath)) {
-          return (
-            fs.readFileSync(filePath, "utf8") +
-            `\nglobalThis.SH_CKE_CONFIG = CKEConfig()`
-          );
-        }
-      }
-
-      return `globalThis.SH_CKE_CONFIG = null`;
+      return config || `globalThis.SH_CKE_CONFIG = null`;
     },
   };
 };
