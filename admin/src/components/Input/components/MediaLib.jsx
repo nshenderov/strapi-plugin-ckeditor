e42f4f7993c10fb8e542d228d9df023bf0bbfe05
nshenderov <103522779+nshenderov@users.noreply.github.com>
2024-11-18 23:59:11 +0300

    feat: fullscreen (#183)

--- admin/src/components/Input/components/MediaLib.jsx
+++ admin/src/components/Input/components/MediaLib.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useStrapiApp } from '@strapi/strapi/admin';
 
 import { prefixFileUrlWithBackendUrl } from '../../../utils/prefixFileUrlWithBackendUrl'
 
-export const MediaLib = ({ isOpen = false, onToggle = () => {}, editor }) => {
+export const MediaLib = ({ isOpen = false, toggle, handleChangeAssets }) => {
   const components = useStrapiApp('MediaLib', ({ components }) => components);
   
   const MediaLibraryDialog = components['media-library'];
 
-  const handleChangeAssets = (assets) => {
-    let newValue = '';
+  const getNewElems = (assets) => {
+    let newElems = '';
 
     assets.map(({ name, url, alt, formats, mime, width, height }) => {
       if (mime.includes('image')) {
@@ -18,25 +17,21 @@ export const MediaLib = ({ isOpen = false, onToggle = () => {}, editor }) => {
           let set = '';
           let keys = Object.keys(formats).sort((a, b) => formats[a].width - formats[b].width);
           keys.map((k) => (set += prefixFileUrlWithBackendUrl(formats[k].url) +` ${formats[k].width}w,`));
-          newValue += `<img src="${url}" alt="${alt}" width="${width}" height="${height}" srcset="${set}" />`;
+          newElems += `<img src="${url}" alt="${alt}" width="${width}" height="${height}" srcset="${set}" />`;
         } else {
-          newValue += `<img src="${url}" alt="${alt}" width="${width}" height="${height}" />`;
+            newElems += `<img src="${url}" alt="${alt}" width="${width}" height="${height}" />`;
         }
       } else if (mime.includes('video')) {
-        newValue += `
+        newElems += `
             <video class="video" controls width="500px">
                 <source src="${url}" type="${mime}" />
             </video>`;
       } else {
-        newValue += `<a href="${url}">${name || 'Open document'}</a>`;
+        newElems += `<a href="${url}">${name || 'Open document'}</a>`;
       }
     });
 
-    const viewFragment = editor.data.processor.toView(newValue);
-    const modelFragment = editor.data.toModel(viewFragment);
-    editor.model.insertContent(modelFragment);
-
-    onToggle();
+    return newElems;
   };
 
   const handleSelectAssets = (files) => {
@@ -50,7 +45,9 @@ export const MediaLib = ({ isOpen = false, onToggle = () => {}, editor }) => {
       height: f.height,
     }));
 
-    handleChangeAssets(formattedFiles);
+    const newElems = getNewElems(formattedFiles);
+
+    handleChangeAssets(newElems);
   };
 
   if (!isOpen) {
@@ -59,13 +56,8 @@ export const MediaLib = ({ isOpen = false, onToggle = () => {}, editor }) => {
 
   return (
     <MediaLibraryDialog
-      onClose={onToggle}
+      onClose={toggle}
       onSelectAssets={handleSelectAssets}
     />
   );
-};
-
-MediaLib.propTypes = {
-  isOpen: PropTypes.bool,
-  onToggle: PropTypes.func,
 };
