467635dfa3fa748d7f158f98a400b79917148673
nshenderov <nkshenderov@gmail.com>
2024-12-08 20:32:37 +0300

    chore: update exports

--- admin/src/exports.ts
+++ admin/src/exports.ts
@@ -1,3 +1,13 @@
-export type * from './config/types';
+export type {
+  UserPluginConfig as PluginConfig,
+  EditorConfig,
+  Theme,
+  Preset,
+  Field,
+  Metadatas,
+  IntlLabel,
+  Styles,
+} from './config/types';
 export { setPluginConfig, defaultHtmlPreset, defaultMarkdownPreset } from './config';
 export { StrapiMediaLib, StrapiUploadAdapter } from './plugins';
+export { defaultTheme } from './theme';
