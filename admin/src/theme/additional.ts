c0b51808282df8a06531b122c0f287d75bb3b742
nshenderov <nkshenderov@gmail.com>
2024-12-11 13:35:19 +0300

    feat: update theme (#193)
    - Reformats the editor theme to use Strapi's theme colors.
    - Updates the dark theme for better alignment with the default editor's appearance.
    
    BREAKING CHANGE:
    If you are using a custom Strapi or editor theme, these updates may introduce changes to the editor's appearance.

--- admin/src/theme/additional.ts
+++ admin/src/theme/additional.ts
@@ -3,6 +3,9 @@ import { css } from 'styled-components';
 const editorSizes = css`
   :root {
     --ck-editor-full-screen-box-max-width: 1700px;
+  }
+
+  .ck {
     --ck-editor-max-width: 1366px;
     --ck-editor-min-height: 200px;
     --ck-editor-max-height: 500px;
